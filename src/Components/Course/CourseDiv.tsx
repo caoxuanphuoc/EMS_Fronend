f3648aa833e26aa7f58467a986d330453593f877
Phuoc <caophuoc6@gmail.com>
2024-05-12 16:07:39 +0700

    add screen home sugesst class and detail class

--- src/Components/Course/CourseDiv.tsx
+++ src/Components/Course/CourseDiv.tsx
@@ -1,26 +1,57 @@
 import React from "react";
 import { BiTimeFive } from 'react-icons/bi'
+import { DateHelpers } from "../../Helpers/DateHelper";
+import { NumberHelpers } from "../../Helpers/NumberHelper"
+import { useNavigate, createSearchParams } from "react-router-dom";
 
-const Course = () => {
+interface classProps {
+    id: number,
+    title: string,
+    img: string,
+    startDate: string,
+    courseName: string,
+    fee: number,
+    showDiscount?: boolean
+}
+const Course = (props: classProps) => {
+    const navigate = useNavigate();
+    const handelNavLink = (idCourse: number) => {
+        navigate(
+            "/detail?id=" + idCourse,
+            {
+                state: {
+                    id: idCourse
+                },
+            }
+        );
+    }
+    const notDiscount = props.fee - (props.fee * 0.1)
     return (
-        <div className="CourseContainer flex gap-10 justify-center flex-wrap items-center m-5">
+        <div className="CourseContainer flex gap-10 justify-center flex-wrap items-center">
             <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] 
-            hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:sha-lg border border-solid border-gray-300    ">
-                <span className=" flex justify-between items-center gap-4">
-                    <h1 className="text-[16px] font-semibold text-textColor
-                     group-hover:text-white"> Web develop</h1>
-                    <span className="flex items-center text-[#ccc] gap-1">
-                        <BiTimeFive /> Now
-                    </span>
+            hover:bg-blue-200  shadow-lg shadow-greyIsh-400/700 hover:sha-lg border border-solid border-gray-300    ">
+                <img src={props.img} alt="" className="rounded-sm" />
+                <span className="items-center my-10">
+                    <h1 className="text-[16px] font-semibold text-textColor mt-3 truncate
+                     " >Lớp: {props.title}</h1>
+                    <h6 className="'text-[#ccc] group-hover:text-black font-semibold"> Môn học: {props.courseName}</h6>
                 </span>
-                <h6 className="'text-[#ccc]"> Canada</h6>
+                <span className="flex items-center  ">
+                    <BiTimeFive className="group-hover:text-green-500 text-green-500 " /> {DateHelpers.formatDate(props.startDate)}
+                </span>
+                <span className="flex gap-2">
+                    <h6 className="text-blue-600 group-hover:text-red-500 font-semibold"> Giá chỉ: {NumberHelpers.formatNumberMoney(props.fee)}</h6>
+                    {
+                        props.showDiscount &&
+                        <h6 className="text-blue-500 line-through group-hover:text-red-400">  {NumberHelpers.formatNumberMoney(notDiscount)}</h6>
+                    }
+                </span>
 
-                <p className="text-[13px] text-[#95959] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, placeat
-                    . Rerum reprehenderit necessitatibus molestias molestiae hic fugiat itaque sit corporis quasi,
-                </p>
-                <button className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-white mt-5">
-                    Đăng ký
+                <ul>
+
+                </ul>
+                <button onClick={() => handelNavLink(props.id)} className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold bg-blue-50 shadow-sm text-textColor hover:bg-blue-600 group-hover/item:text-textColor group-hover:text-white group-hover:bg-orange-500 mt-5">
+                    Đăng ký ngay
                 </button>
             </div>
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Course from './Components/Course/CourseDiv.tsx'
 import SearchBar from './Components/SearchDiv/Search.tsx'
-import HomePage from './Components/Home/HomePage.tsx'
+import HomePage from './Scenes/Home/HomePage.tsx'
 import Login from './Scenes/Home/Login.tsx'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import TestPage from './Scenes/Test/pageTest.tsx'
+import ClassDetailScreen from './Scenes/Class/ClassDetailScreen.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
@@ -17,8 +19,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path='/' element={<App />} >
             <Route index element={<HomePage />} />
-            <Route path='/course' element={<Course />} />
+            <Route path='/detail' element={<ClassDetailScreen />} />
             <Route path='/about' element={<SearchBar />} />
+            <Route path='/testing' element={<TestPage />} />
           </Route>
           <Route path='/auth' element={<Login isRegister={false} />} />
           <Route path='/register' element={<Login isRegister={true} />} />
