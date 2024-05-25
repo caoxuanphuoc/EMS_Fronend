e2acf5ceb14e8163a698d6d5d7966deb0699f297
Admin <caophuoc6@gmail.com>
2024-05-25 14:30:24 +0700

    Delete proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: "http://103.116.8.38:44311/api/", //"https://localhost:44311/api", //"http://ecms.io.vn:44311/api/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      }
-    }
-  }
 })
